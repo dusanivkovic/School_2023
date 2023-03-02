e4f9197f53892cb510264f15898b7326971c179d
dusanivkovic <ivdule77@gmail.com>
2023-03-02 10:25:42 +0100

    Adding Owl Carousel

--- script/main.js
+++ script/main.js
@@ -48,9 +48,30 @@ const typingHeding = () => {
   }
 }
 typingHeding();
+//Counter
 $('.counter').counterUp({
   delay: 10,
   time: 2000
 });
 $('.counter').addClass('animated fadeInDownBig');
 $('h3').addClass('animated fadeIn');
+//Owl Carousel
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
